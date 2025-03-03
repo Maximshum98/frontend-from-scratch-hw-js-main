ff78fdddddcbfc4d1acb014d2580bd9c26aa783d
Maxim Shumeyko <maximshum98@yandex.ru>
2025-03-03 23:32:08 +0300

    first commit

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,7 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(numb, numb1) {
+   return (numb+numb1) / 2
+}
+const number = calculateAverage(45,85)
+console.log(number);
--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,3 +1,18 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
+let result;
+function findLargest(number1, number2, number3) {
+    
+    if (number1 >= number2 && number1 >= number3) {
+    result = number1; 
+    } else if (number2 >= number1 && number2 >= number3) {
+    result = number2; 
+    } else {
+            result = number3;
+        }  
+        console.log(result);
+        
+    }    
+    findLargest(56, 87, 86)
+    
 
-function findLargest() {}
+
