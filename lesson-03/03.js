84006383016001ce18dfb8dd4f22b600f795ede2
Maxim Shumeyko <maximshum98@yandex.ru>
2025-03-04 22:54:48 +0300

    two commit

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,18 +1,16 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(number1, number2, number3) {
-    let result;
     if (number1 >= number2 && number1 >= number3) {
-    result = number1; 
+    return number1; 
     } else if (number2 >= number1 && number2 >= number3) {
-    result = number2; 
+        return number2; 
     } else {
-            result = number3;
+        return number3; 
         }  
-        console.log(result);
-        
     }    
-    findLargest(1, 2, 3)
+    console.log(findLargest(25, 105, 855559));
+     
     
 
 
